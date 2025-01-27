ea768b9fed188e280e067c4023d85ec921c1a317
n-ruranga <n.ruranga@alustudent.com>
2025-01-27 10:40:05 +0200

    initial commit

---
+++ the_coder.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#The following line is for creating a dir called "students_directory"
+mkdir students_directory
+
+#In the dir above, we are addint three txt files  using the following command
+touch students_directory/cohort-1.txt
+touch students_directory/cohort-2.txt
+touch students_directory/cohort-3.txt
+
